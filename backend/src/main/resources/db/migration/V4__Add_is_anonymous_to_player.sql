66ba97bda05641165f18295beb78bc2c9486b2b5
hawkeye <wes.maszk@rocketpartners.io>
2025-08-25 19:53:29 -0400

    trying something

--- backend/src/main/resources/db/migration/V4__Add_is_anonymous_to_player.sql
+++
@@ -1,5 +0,0 @@
--- Add is_anonymous column to player table
-ALTER TABLE player ADD COLUMN is_anonymous BOOLEAN DEFAULT FALSE;
-
--- Add index for anonymous player queries
-CREATE INDEX idx_player_anonymous ON player(is_anonymous);
